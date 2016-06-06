b6516e586dcc07406949bfb675873b9986509615
Artyom Lyan <avn8068@gmail.com>
2016-06-06 10:26:05 -0400

    initial draft update

--- src/com/company/CustomMapper.java
+++ src/com/company/CustomMapper.java
@@ -1,19 +1,35 @@
 package com.company;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 /**
  * Created by arli on 6/6/16.
  */
-public class CustomMapper extends MapReduceBase implements Mapper {
+public class CustomMapper extends MapReduceBase implements
+        Mapper <LongWritable, Text, Text, IntWritable>{
 
     @Override
-    public void map(Object o, Object o2, OutputCollector outputCollector, Reporter reporter) throws IOException {
+    public void map(LongWritable longWritable, Text text, OutputCollector<Text, IntWritable> outputCollector, Reporter reporter) throws IOException {
+        String line = text.toString();
+        String lastToken = "";
+        StringTokenizer tokenizer = new StringTokenizer(line, "/t");
 
+        String year = tokenizer.nextToken();
+
+        while(tokenizer.hasMoreTokens()){
+            lastToken = tokenizer.nextToken();
+        }
+
+        int avgPrice = Integer.parseInt(lastToken);
+        outputCollector.collect(new Text(year), new IntWritable(avgPrice));
     }
 }
--- src/com/company/CustomReducer.java
+++ src/com/company/CustomReducer.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reducer;
@@ -11,10 +13,20 @@ import java.util.Iterator;
 /**
  * Created by arli on 6/6/16.
  */
-public class CustomReducer extends MapReduceBase implements Reducer {
+public class CustomReducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
 
     @Override
-    public void reduce(Object o, Iterator iterator, OutputCollector outputCollector, Reporter reporter) throws IOException {
+    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> outputCollector, Reporter reporter) throws IOException {
+        int maxAvg = 30;
+        int val = Integer.MIN_VALUE;
 
+        while(values.hasNext()){
+            val = values.next().get();
+
+            if (val > maxAvg){
+                outputCollector.collect(key, new IntWritable(val));
+            }
+
+        }
     }
 }
